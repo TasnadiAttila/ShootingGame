9b138ffcea8738e88e577d02529c86e75c386708
TasnadiAttila <70896019+TasnadiAttila@users.noreply.github.com>
2022-03-04 17:01:13 +0100

    First Steps on a long long way for Disk shooter

---
+++ Assets/cannon.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cannon : MonoBehaviour
+{
+    [SerializeField]//editorba lehet nyomni
+    GameObject disk;
+
+    [SerializeField]
+    float timeBetweenShots;
+
+    [SerializeField]
+    float firePower;
+
+    float timeLeft;//0 és 2 között mindig és változik, ennyi időnként nő
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        timeLeft = timeBetweenShots; 
+    }
+
+    // Update is called once per frame
+    //
+    private void Update()
+    {
+        //timeleft hány sec múlva fog lőni
+        timeLeft -= Time.deltaTime;//deltaTime 2 frame között eltelt idő
+        if(timeLeft<=0){
+            timeLeft = timeBetweenShots;
+            Shoot();
+        }
+    }
+
+
+    void Shoot(){
+        GameObject diskObject = Instantiate(disk,transform.position,Quaternion.identity);//Unity-n belül a transformos,igazodik az object szögéhez
+        diskObject.GetComponent<Rigidbody>().AddForce(transform.up* firePower,ForceMode.Impulse);//Rigidbody ilyen FIZIKA, olyan mintha meglöknénk
+                                                                                //ForceMode csak 1x adja hozzá az erőt nem folyomatosan mint egy ütés
+        Destroy(diskObject, 5f);
+    }
+}
